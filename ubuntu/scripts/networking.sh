9b02b53596986a7d97349059155a1abc9d4f3af4
Jose Riguera <jriguera@gmail.com>
2015-10-14 21:33:27 +0200

    adding bonding module

--- ubuntu/scripts/networking.sh
+++ ubuntu/scripts/networking.sh
@@ -6,6 +6,9 @@ apt-get install -y vlan
 echo "* Adding vlan module to modules"
 grep -q 8021q /etc/modules || echo 8021q >> /etc/modules
 
+echo "* Adding bonding to modules"
+grep -q bonding /etc/modules || echo bonding >> /etc/modules
+
 echo "* Installing bridge-utils for linux bridges"
 apt-get install -y bridge-utils
 
