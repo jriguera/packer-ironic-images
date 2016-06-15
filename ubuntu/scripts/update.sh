c104f53c1ec3ba304d786c4c9d3cede8721e992b
Jose Riguera <jriguera@gmail.com>
2016-06-16 01:35:48 +0200

    fix dist-upgrade

--- ubuntu/scripts/update.sh
+++ ubuntu/scripts/update.sh
@@ -8,8 +8,6 @@ apt-get -y update
 
 if [ -z "${UPGRADE##*true*}" ] || [ -z "${UPGRADE##*1*}" ] || [ -z "${UPGRADE##*yes*}" ]; then
     echo "* Performing upgrade (all packages and kernel)"
-    apt-get -y dist-upgrade --force-yes
-    reboot
-    sleep 60
+    apt-get -y dist-upgrade --allow-change-held-packages
+    sync
 fi
-
